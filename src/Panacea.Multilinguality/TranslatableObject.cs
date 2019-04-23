d111900ab903feda8626afeaefb73df49982be11
Portno <j.remoundos@dotbydot.gr>
2019-04-23 12:30:51 +0300

    added translatable object

---
+++ src/Panacea.Multilinguality/TranslatableObject.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Panacea.Multilinguality
+{
+    public class TranslatableObject : INotifyPropertyChanged
+    {
+        private string _text;
+        private readonly Translator _translator;
+        private readonly object[] _args;
+
+        public int MaxChars { get; set; } = int.MaxValue;
+
+        public TranslatableObject()
+        {
+        }
+
+        public TranslatableObject(string realText)
+        {
+            RealText = realText;
+            Text = realText;
+        }
+
+        public TranslatableObject(string text, Translator translator, params object[] args)
+        {
+            _translator = translator;
+            _args = args;
+            Text = string.Format(translator.Translate(text), args);
+            WeakEventManager<LanguageContext, PropertyChangedEventArgs>
+                .AddHandler(LanguageContext.Instance,
+                            nameof(LanguageContext.PropertyChanged),
+                            OnLanguageContextPropertyChanged);
+
+        }
+
+        private void OnLanguageContextPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Culture")
+            {
+                Text = string.Format(_translator?.Translate(_text), _args);
+            }
+        }
+
+        public TranslatableObject(dynamic obj, params string[] fields)
+        {
+            dynamic obj2 = obj;
+            foreach (string s in fields)
+            {
+                obj2 = obj2[s];
+            }
+            try
+            {
+                dynamic trans = null;
+                try
+                {
+                    trans = obj.trans;
+                    foreach (string s in fields)
+                    {
+                        if (trans != null)
+                            trans = trans[s];
+                    }
+                }
+                catch
+                {
+                }
+                if (trans != null)
+                {
+                    WeakEventManager<LanguageContext, PropertyChangedEventArgs>
+                        .AddHandler(LanguageContext.Instance,
+                           nameof(LanguageContext.PropertyChanged),
+                           (oo, ee) =>
+                    {
+                        try
+                        {
+                            if (ee.PropertyName == "Culture")
+                            {
+                                Text = trans[LanguageContext.Instance.Culture.Name].Value;
+                            }
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                Text = obj2.Value;
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    });
+                    try
+                    {
+                        Text = trans[LanguageContext.Instance.Culture.Name].Value;
+                    }
+                    catch
+                    {
+                        Text = obj2.Value;
+                    }
+                }
+                else
+                {
+                    Text = obj2.Value;
+                }
+                RealText = obj2.Value;
+            }
+            catch
+            {
+            }
+        }
+
+        public string RealText { get; set; }
+
+        public string Text
+        {
+            get
+            {
+                if (MaxChars != int.MaxValue)
+                    return SmartTextCrop(_text, MaxChars);
+                return _text;
+            }
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    OnPropertyChanged("Text");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string SmartTextCrop(string text, int maxChars)
+        {
+            if (maxChars < text.Length)
+            {
+                text = text.Substring(0, maxChars);
+                int Space = text.LastIndexOf(" ");
+                int LastDot = text.LastIndexOf(".");
+                if (Space > LastDot)
+                {
+                    return text.Substring(0, Space) + "...";
+                }
+                else
+                {
+                    return text.Substring(0, LastDot);
+                }
+            }
+            return text;
+        }
+    }
+}
---
+++ src/Panacea.Multilinguality/Translator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Panacea.Multilinguality
+{
+    public class Translator
+    {
+        private readonly string _namespac;
+
+        public Translator(string namespac)
+        {
+            _namespac = namespac;
+        }
+
+        public string Translate(string text)
+        {
+            return LanguageContext.Instance.Dictionary.Translate(text, _namespac);
+        }
+
+        public string Translate(string text, params object[] args)
+        {
+            return string.Format(LanguageContext.Instance.Dictionary.Translate(text, _namespac), args);
+        }
+
+        public void CreateBinding(DependencyProperty property, FrameworkElement element, string text,
+            params object[] args)
+        {
+            BindingOperations.ClearBinding(element, property);
+            var myBinding = new Binding(nameof(TranslatableObject.Text));
+            var obj = new TranslatableObject(text, this, args);
+            myBinding.Source = obj;
+            element.SetBinding(property, myBinding);
+        }
+
+        public void CreateBindingJson(DependencyProperty property, FrameworkElement element, dynamic text)
+        {
+            BindingOperations.ClearBinding(element, property);
+            var myBinding = new Binding(nameof(TranslatableObject.Text));
+            var obj = new TranslatableObject(text, this);
+            myBinding.Source = obj;
+            element.SetBinding(property, myBinding);
+        }
+    }
+
+   
+}
